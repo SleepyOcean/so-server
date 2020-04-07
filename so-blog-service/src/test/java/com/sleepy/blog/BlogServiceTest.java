4d1b2ababed02a02b0218bea9e7bb5bb8bc59d0c
gehoubao <gehoubao@kedacom.com>
2020-04-07 16:46:23 +0800

    【部署调整】暂时去除“so-security”模块

--- so-blog-service/src/test/java/com/sleepy/blog/BlogServiceTest.java
+++ so-blog-service/src/test/java/com/sleepy/blog/BlogServiceTest.java
@@ -1,21 +1,10 @@
 package com.sleepy.blog;
 
-import com.sleepy.blog.entity.TagEntity;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Query;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author gehoubao
